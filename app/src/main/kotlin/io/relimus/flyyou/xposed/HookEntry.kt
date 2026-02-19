b428c97aaf825034ec1eb513e4cdee8dc3cef339
relimus <relimus@proton.me>
2026-02-19 16:41:40 +0800

    feat: 添加 Hook 总入口类

---
+++ app/src/main/kotlin/io/relimus/flyyou/xposed/HookEntry.kt
@@ -0,0 +1,21 @@
+package io.relimus.flyyou.xposed
+
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+import io.relimus.flyyou.xposed.hooks.RemoveGamePanelGift
+import io.relimus.flyyou.xposed.hooks.SettingGMSActivity
+
+@InjectYukiHookWithXposed(entryClassName = "Entry")
+object HookEntry : IYukiHookXposedInit {
+    override fun onInit() = configs {
+        debugLog { tag = "FlyYou" }
+        isDebug = false
+        isEnableDataChannel = false
+    }
+    override fun onHook() = encase {
+        loadApp("com.flyme.systemuitools", RemoveGamePanelGift)
+        loadApp("com.android.settings", SettingGMSActivity)
+    }
+}
