41dd168dca6f6022a964ebd15f53ca09d66aef22
relimus <relimus@proton.me>
2026-04-26 17:28:21 +0800

    chore: 去除 UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
 }
 
@@ -15,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "io.relimus.flyyou"
         minSdk = 33
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
@@ -38,7 +37,6 @@ android {
     }
     buildFeatures {
         buildConfig = true
-        compose = true
     }
 }
 
@@ -47,19 +45,7 @@ dependencies {
     implementation(libs.yukihook.api)
     ksp(libs.yukihook.ksp.xposed)
     implementation(libs.kavaref.core)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
